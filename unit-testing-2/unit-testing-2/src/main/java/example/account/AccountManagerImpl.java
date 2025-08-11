a4f76c86ff3b8d1d15d7e33d7d9ab821da2289ce
Orangekun <amazingritro66@gmail.com>
2025-08-12 01:32:07 +0300

    GREEN

--- unit-testing-2/unit-testing-2/src/main/java/example/account/AccountManagerImpl.java
+++ unit-testing-2/unit-testing-2/src/main/java/example/account/AccountManagerImpl.java
@@ -3,12 +3,18 @@ package example.account;
 public class AccountManagerImpl implements AccountManager {
     @Override
     public void deposit(Customer customer, int amount) {
-        
+        customer.setBalance(customer.getBalance() + amount);
     }
 
     @Override
     public String withdraw(Customer customer, int amount) {
-        customer.setBalance(customer.getBalance() - amount);
+        int expectedBalance = customer.getBalance() - amount;
+        
+        if (expectedBalance < 0) {
+            return "insufficient account balance";
+        }
+        
+        customer.setBalance(expectedBalance);
         return "success";
     }
 }
